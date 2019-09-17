56468beb3c1e605cd4e77fe780c4a8574879ed25
Hiram K <hiram.kamau@outlook.com>
2019-09-17 12:11:03 +0300

    Change type of pane to vbox

--- many-faces-ui/src/main/java/com/manyfaces/ui/BrowserProfileList.java
+++ many-faces-ui/src/main/java/com/manyfaces/ui/BrowserProfileList.java
@@ -3,10 +3,59 @@
  */
 package com.manyfaces.ui;
 
+import com.manyfaces.ui.controllers.PageHeaderController;
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
 /**
 
  @author Hiram K <hiram.kamau@outlook.com>
  */
-public class BrowserProfileList {
-    
+public class BrowserProfileList extends VBox {
+
+    private static final Logger LOG;
+
+    static {
+        LOG = Logger.getLogger(BrowserProfileList.class.getName());
+    }
+
+    /**
+     Default constructor.
+     */
+    public BrowserProfileList() {
+        super();
+    }
+
+    public Pane loadPane() {
+        Pane headerPane = null;
+
+        try {
+            headerPane = loadHeaderPane();
+        } catch (IOException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+        }
+
+        if (headerPane != null) {
+            getChildren().setAll(headerPane);
+        }
+        
+        return this;
+    }
+
+    private Pane loadHeaderPane() throws IOException {
+        URL location = getClass().getResource("/views/PageHeader.fxml");
+        FXMLLoader loader = new FXMLLoader(location);
+        Pane pane = loader.load();
+        PageHeaderController controller = loader.getController();
+
+        controller.setHeaderText("Browser profile list");
+
+        return pane;
+    }
+
 }
