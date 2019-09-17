b18bb55553b498b12fdcdaa35f68774334dfeef6
Hiram K <hiram.kamau@outlook.com>
2019-09-17 10:57:59 +0300

    Change pane loaded to scene

--- many-faces-ui/src/main/java/com/manyfaces/Main.java
+++ many-faces-ui/src/main/java/com/manyfaces/Main.java
@@ -7,7 +7,6 @@ import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -28,9 +27,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        AnchorPane pane = new AnchorPane();
-
-        Scene scene = new Scene(pane);
+        Scene scene = new Scene(new Home().loadPane());
 
         primaryStage.setOnShown(e -> maximizeFallBack(primaryStage));
         primaryStage.setTitle("Many Faces");
