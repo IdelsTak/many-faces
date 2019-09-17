edfe83edcebe34a04498fd6c32efb642eed56635
Hiram K <hiram.kamau@outlook.com>
2019-09-17 13:52:31 +0300

    Declare controls

--- many-faces-ui/src/main/java/com/manyfaces/ui/controllers/ProfileListTabController.java
+++ many-faces-ui/src/main/java/com/manyfaces/ui/controllers/ProfileListTabController.java
@@ -3,7 +3,10 @@
  */
 package com.manyfaces.ui.controllers;
 
+import com.jfoenix.controls.JFXTabPane;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
 
 /**
  FXML Controller class
@@ -12,12 +15,23 @@ import javafx.fxml.FXML;
  */
 public class ProfileListTabController {
 
+    private static final Logger LOG;
+    @FXML
+    private JFXTabPane tabPane;
+    @FXML
+    private Tab profileListTab;
+    @FXML
+    private Tab groupsTab;
+
+    static {
+        LOG = Logger.getLogger(ProfileListTabController.class.getName());
+    }
+
     /**
      Initializes the controller class.
      */
     @FXML
     public void initialize() {
-        // TODO
-    }    
-    
+//        LOG.log(Level.INFO, "Style: {0}", tabPane);
+    }
 }
