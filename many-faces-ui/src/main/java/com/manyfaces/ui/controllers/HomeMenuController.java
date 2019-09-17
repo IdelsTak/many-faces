afe5b53c66384e61d74204c697439fce48bee6e9
Hiram K <hiram.kamau@outlook.com>
2019-09-17 10:58:54 +0300

    Remove 'radio-button' styling to make the buttons look like toggle buttons while retaining all the functionality of the radio button

--- many-faces-ui/src/main/java/com/manyfaces/ui/controllers/HomeMenuController.java
+++ many-faces-ui/src/main/java/com/manyfaces/ui/controllers/HomeMenuController.java
@@ -3,7 +3,12 @@
  */
 package com.manyfaces.ui.controllers;
 
+import java.util.logging.Logger;
+import javafx.css.Styleable;
 import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
 
 /**
  FXML Controller class
@@ -12,12 +17,34 @@ import javafx.fxml.FXML;
  */
 public class HomeMenuController {
 
+    private static final Logger LOG;
+    @FXML
+    private ToggleGroup menuGroup;
+    @FXML
+    private RadioButton homeToggle;
+    @FXML
+    private RadioButton newProfileToggle;
+    @FXML
+    private RadioButton accountToggle;
+    @FXML
+    private RadioButton pluginsToggle;
+    @FXML
+    private RadioButton helpToggle;
+
+    static {
+        LOG = Logger.getLogger(HomeMenuController.class.getName());
+    }
+
     /**
      Initializes the controller class.
      */
     @FXML
     public void initialize() {
-        // TODO
-    }    
-    
+        menuGroup.getToggles().forEach(this::removeRadioButtonStyling);
+    }
+
+    private void removeRadioButtonStyling(Toggle toggle) {
+        ((Styleable) toggle).getStyleClass().remove("radio-button");
+    }
+
 }
