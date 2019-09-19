cef32e757fe88b895424bf1ce4fa620375b24e75
Hiram K <hiram.kamau@outlook.com>
2019-09-19 18:57:39 +0300

    Consume any action that attempts to expand the titledpane

--- many-faces-ui/src/main/java/com/manyfaces/ui/controllers/GroupListRowController.java
+++ many-faces-ui/src/main/java/com/manyfaces/ui/controllers/GroupListRowController.java
@@ -3,23 +3,49 @@
  */
 package com.manyfaces.ui.controllers;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
+import javafx.css.Styleable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TitledPane;
+import javafx.scene.input.InputEvent;
+import javafx.scene.layout.HBox;
 
 /**
  FXML Controller class
 
  @author Hiram K <hiram.kamau@outlook.com>
  */
-public class GroupListRowController implements Initializable {
+public class GroupListRowController {
+
+    @FXML
+    private TitledPane titledPane;
+    @FXML
+    private HBox titleHbox;
+    @FXML
+    private Label groupNameLabel;
+    @FXML
+    private Label noOfProfilesLabel;
 
     /**
      Initializes the controller class.
      */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+    @FXML
+    public void initialize() {
+        titledPane.addEventFilter(InputEvent.ANY, e -> {
+            //If the target of the event is the title, ignore it
+            //we want to be able to expand the titled pane using 
+            //our custom button only
+            if (((Styleable) e.getTarget())
+                    .getStyleClass()
+                    .contains("title")) {
+                e.consume();
+            }
+        });
+        titleHbox.prefWidthProperty().bind(titledPane.widthProperty());
+    }
+
+    void setGroupName(String groupName) {
+        groupNameLabel.setText(groupName);
+    }
+
 }
