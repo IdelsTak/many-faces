91f68e76e0aad734173fb8d5baee5b987ddaa018
Hiram K <hiram.kamau@outlook.com>
2019-09-20 18:17:20 +0300

    Add close dialog actions

--- many-faces-ui/src/main/java/com/manyfaces/ui/controllers/EditGroupsDialogController.java
+++ many-faces-ui/src/main/java/com/manyfaces/ui/controllers/EditGroupsDialogController.java
@@ -3,23 +3,36 @@
  */
 package com.manyfaces.ui.controllers;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDialog;
+import javafx.fxml.FXML;
 
 /**
  FXML Controller class
 
  @author Hiram K <hiram.kamau@outlook.com>
  */
-public class EditGroupsDialogController implements Initializable {
+public class EditGroupsDialogController {
+
+    @FXML
+    private JFXButton closeButton;
+    @FXML
+    private JFXButton cancelButton;
 
     /**
      Initializes the controller class.
      */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    @FXML
+    public void initialize() {
         // TODO
-    }    
-    
+    }
+
+    void setDialog(JFXDialog dialog) {
+        if (dialog == null) {
+            throw new IllegalArgumentException("Dialo should not be null");
+        }
+        closeButton.setOnAction(e -> dialog.close());
+        cancelButton.setOnAction(e -> dialog.close());
+    }
+
 }
