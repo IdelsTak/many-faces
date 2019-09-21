11ce04e90c8d2f9543c38b985801b05ebc58e323
Hiram K <hiram.kamau@outlook.com>
2019-09-21 12:45:14 +0300

    Access groups using lookup

--- many-faces-ui/src/main/java/com/manyfaces/ui/controllers/EditGroupsDialogController.java
+++ many-faces-ui/src/main/java/com/manyfaces/ui/controllers/EditGroupsDialogController.java
@@ -8,21 +8,24 @@ import com.jfoenix.controls.JFXDialog;
 import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXTextField;
 import com.manyfaces.model.Group;
+import com.manyfaces.spi.GroupsRepository;
 import java.io.IOException;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.application.Platform;
 import javafx.beans.binding.BooleanBinding;
 import javafx.beans.property.BooleanProperty;
-import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener.Change;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
+import org.openide.util.Lookup;
 
 /**
  FXML Controller class
@@ -58,8 +61,6 @@ public class EditGroupsDialogController {
      */
     @FXML
     public void initialize() {
-        LOG.log(Level.INFO, "list style: {0}", groupsListView.getStyleClass());
-
         BooleanProperty nameFieldprop = groupNameField.visibleProperty();
         BooleanBinding nameFieldNotProp = nameFieldprop.not();
 
@@ -68,29 +69,27 @@ public class EditGroupsDialogController {
 
         addNewgroupButton.setOnAction(e -> groupNameField.setVisible(true));
 
-        groupsListView.setCellFactory(listView -> new GroupCell());
+        Lookup globalLookup = Lookup.getDefault();
+        GroupsRepository repository = globalLookup.lookup(GroupsRepository.class);
 
-        ObservableList<Group> groups = FXCollections.observableArrayList();
+        ObservableList<Group> groups = repository.findAll();
 
         groups.addListener((Change<? extends Group> c) -> {
+            LOG.log(Level.INFO, "Groups list change event occured: {0}", c);
+
             noGroupsFoundText.setVisible(c.getList().isEmpty());
             groupsListView.setVisible(!c.getList().isEmpty());
+
+//            groupsListView.setCellFactory(listView -> new GroupCell());
+//            groupsListView.getItems().setAll(groups);
+//            groupsListView.refresh();
+            Platform.runLater(() -> initListView(groups));
         });
 
         noGroupsFoundText.setVisible(groups.isEmpty());
-        groupsListView.setVisible(groups.isEmpty());
+        groupsListView.setVisible(!groups.isEmpty());
 
-        groups.add(new Group("Unassigned"));
-        groups.add(new Group("test group"));
-//        groups.add(new Group("Unassigned"));
-//        groups.add(new Group("test group"));
-//        groups.add(new Group("Unassigned"));
-//        groups.add(new Group("test group"));
-//        groups.add(new Group("Unassigned"));
-//        groups.add(new Group("test group"));
-//        groups.add(new Group("Unassigned"));
-//        groups.add(new Group("test group"));
-        groupsListView.getItems().setAll(groups);
+        initListView(groups);
     }
 
     void setDialog(JFXDialog dialog) {
@@ -99,6 +98,11 @@ public class EditGroupsDialogController {
         }
         closeButton.setOnAction(e -> dialog.close());
         cancelButton.setOnAction(e -> dialog.close());
+    }
+
+    private void initListView(ObservableList<Group> groups) {
+        groupsListView.setCellFactory(listView -> new GroupCell());
+        groupsListView.getItems().setAll(groups);
     }
 
     private static class GroupCell extends ListCell<Group> {
@@ -110,19 +114,25 @@ public class EditGroupsDialogController {
         protected void updateItem(Group group, boolean empty) {
             super.updateItem(group, empty);
 
-            if (group != null) {
-                setGraphic(getHBox(group.getGroupNameProperty().get()));
+            LOG.log(Level.INFO,
+                    "Cell updating... group: {0}; is empty? {1}",
+                    new Object[]{group, empty});
+
+            if (group != null && !empty) {
+                setGraphic(getHBox(group));
+            } else {
+                setGraphic(new HBox(new Label("<empty>")));
             }
         }
 
-        private HBox getHBox(String groupName) {
+        private HBox getHBox(Group group) {
             URL location = getClass().getResource("/views/GroupListCell.fxml");
             FXMLLoader loader = new FXMLLoader(location);
             HBox hBox = null;
             try {
                 hBox = loader.load();
                 GroupListCellController controller = loader.getController();
-                controller.setGroupName(groupName);
+                controller.setGroup(group);
             } catch (IOException ex) {
                 LOG.log(Level.SEVERE, null, ex);
             }
