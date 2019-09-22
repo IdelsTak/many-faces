f157967bde1e46f666feee6c23249f4a10dd3d77
Hiram K <hiram.kamau@outlook.com>
2019-09-22 06:33:12 +0300

    Move JFXPopup initialization out of setOnAction()

--- many-faces-ui/src/main/java/com/manyfaces/ui/controllers/ProfilePaneController.java
+++ many-faces-ui/src/main/java/com/manyfaces/ui/controllers/ProfilePaneController.java
@@ -8,6 +8,7 @@ import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXPopup;
 import com.jfoenix.controls.JFXToggleNode;
+import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.fxml.FXML;
@@ -49,7 +50,7 @@ public class ProfilePaneController {
     @FXML
     public void initialize() {
         titledPane.widthProperty().addListener((obs, ov, nv) -> {
-            titleHbox.setPrefWidth(nv.doubleValue() - 0.0);
+            titleHbox.setPrefWidth(nv.doubleValue());
         });
 
         titledPane.expandedProperty().addListener((obs, ov, nv) -> {
@@ -60,36 +61,38 @@ public class ProfilePaneController {
             titledPane.setExpanded(nv);
         });
 
+        URL styleUrl = getClass().getResource("/styles/profile-pane.css");
+        String style = styleUrl.toExternalForm();
+        JFXListView<Label> list = new JFXListView<>();
+        list.getStylesheets().add(style);
+
+        list.getItems().add(new Label("Edit"));
+        list.getItems().add(new Label("Move to a group"));
+        list.getItems().add(new Label("Delete"));
+
+        list.selectionModelProperty()
+                .getValue()
+                .selectedItemProperty()
+                .addListener((ob, ov, nv) -> {
+                    switch (nv.getText()) {
+                        case "Edit":
+                            LOG.log(Level.INFO, "Edit");
+                            return;
+                        case "Move to a group":
+                            LOG.log(Level.INFO, "Move to a group");
+                            return;
+                        case "Delete":
+                            LOG.log(Level.INFO, "Delete");
+                            return;
+                        default:
+                            throw new IllegalArgumentException("Action not known");
+
+                    }
+                });
+
+        JFXPopup popup = new JFXPopup(list);
+        
         menuButton.setOnAction(e -> {
-            JFXListView<Label> list = new JFXListView<>();
-            list.getStylesheets().add(getClass().getResource("/styles/profile-pane.css").toExternalForm());
-
-            list.getItems().add(new Label("Edit"));
-            list.getItems().add(new Label("Move to a group"));
-            list.getItems().add(new Label("Delete"));
-
-            list.selectionModelProperty()
-                    .getValue()
-                    .selectedItemProperty()
-                    .addListener((ob, ov, nv) -> {
-                        switch (nv.getText()) {
-                            case "Edit":
-                                LOG.log(Level.INFO, "Edit");
-                                return;
-                            case "Move to a group":
-                                LOG.log(Level.INFO, "Move to a group");
-                                return;
-                            case "Delete":
-                                LOG.log(Level.INFO, "Delete");
-                                return;
-                            default:
-                                throw new IllegalArgumentException("Action not known");
-
-                        }
-                    });
-
-            JFXPopup popup = new JFXPopup(list);
-
             popup.show(menuButton,
                     JFXPopup.PopupVPosition.TOP,
                     JFXPopup.PopupHPosition.RIGHT);
