d6d146ec4640808705e53c274730f9756a8bec25
Hiram K <hiram.kamau@outlook.com>
2019-09-23 17:37:23 +0300

    Add functionality
    - Add 'MyAccount.fxml" view to the "My account" 
    tab

--- many-faces-ui/src/main/java/com/manyfaces/ui/controllers/AccountPreferencesTabController.java
+++ many-faces-ui/src/main/java/com/manyfaces/ui/controllers/AccountPreferencesTabController.java
@@ -4,8 +4,12 @@
 package com.manyfaces.ui.controllers;
 
 import com.jfoenix.controls.JFXTabPane;
+import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.SingleSelectionModel;
 import javafx.scene.control.Tab;
 
@@ -25,25 +29,31 @@ public class AccountPreferencesTabController {
 
     /**
      Initializes the controller class.
+
+     @throws java.io.IOException
      */
     @FXML
-    public void initialize() {
-        // TODO
+    public void initialize() throws IOException {
+        URL location = getClass().getResource("/views/MyAccount.fxml");
+        FXMLLoader loader = new FXMLLoader(location);
+        Node accountPane = loader.load();
+        
+        accountTab.setContent(accountPane);
     }
 
     public void setPageHeaderController(PageHeaderController phc) {
         String message = "Header controller should not be null";
         PageHeaderController controller = Objects.requireNonNull(phc, message);
-        
+
         SingleSelectionModel<Tab> ssm = tabPane.getSelectionModel();
-        
+
         ssm.selectedIndexProperty().addListener((o, ov, nv) -> {
             setHeaderText(nv.intValue(), controller);
         });
 
         setHeaderText(ssm.getSelectedIndex(), controller);
     }
-    
+
     private void setHeaderText(int idx, PageHeaderController controller) {
         switch (idx) {
             case 0:
