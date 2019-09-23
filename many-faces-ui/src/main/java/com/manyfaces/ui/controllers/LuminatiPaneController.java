f7562aec5f53c4a013f1a24826b20c3e768bb8f8
Hiram K <hiram.kamau@outlook.com>
2019-09-23 09:59:31 +0300

    Add sample notification

--- many-faces-ui/src/main/java/com/manyfaces/ui/controllers/LuminatiPaneController.java
+++ many-faces-ui/src/main/java/com/manyfaces/ui/controllers/LuminatiPaneController.java
@@ -3,7 +3,16 @@
  */
 package com.manyfaces.ui.controllers;
 
+import com.jfoenix.controls.JFXButton;
+import com.manyfaces.spi.RootComponent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import org.controlsfx.control.Notifications;
+import org.openide.util.Lookup;
 
 /**
  FXML Controller class
@@ -12,12 +21,37 @@ import javafx.fxml.FXML;
  */
 public class LuminatiPaneController {
 
+    private static final Logger LOG;
+    @FXML
+    private AnchorPane rootPane;
+    @FXML
+    private JFXButton activateButton;
+
+    static {
+        LOG = Logger.getLogger(LuminatiPaneController.class.getName());
+    }
+
     /**
      Initializes the controller class.
      */
     @FXML
     public void initialize() {
-        // TODO
+        StackPane root = Lookup.getDefault()
+                .lookup(RootComponent.class)
+                .getRoot();
+
+        Notifications notifications = Notifications.create()
+                .owner(root)
+                .text("Hello notification")
+                .position(Pos.TOP_CENTER);
+
+        activateButton.setOnAction(e -> {
+            LOG.log(Level.INFO,
+                    "\n++++++++++++++++++++++++++++"
+                    + "\nActivate button clicked"
+                    + "\n++++++++++++++++++++++++++++");
+            notifications.show();
+        });
     }
 
 }
