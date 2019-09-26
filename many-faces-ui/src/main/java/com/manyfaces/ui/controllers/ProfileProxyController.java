e44d7f55954ea169cb9e6e1cd828ce73f16f46c6
Hiram K <hiram.kamau@outlook.com>
2019-09-26 20:21:56 +0300

    Add functionality
    - Add bindings
    - Add pre-defined proxy values to proxy combobox

--- many-faces-ui/src/main/java/com/manyfaces/ui/controllers/ProfileProxyController.java
+++ many-faces-ui/src/main/java/com/manyfaces/ui/controllers/ProfileProxyController.java
@@ -3,6 +3,10 @@
  */
 package com.manyfaces.ui.controllers;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXToggleNode;
 import javafx.fxml.FXML;
 
 /**
@@ -12,12 +16,59 @@ import javafx.fxml.FXML;
  */
 public class ProfileProxyController {
 
+    @FXML
+    private JFXComboBox<Proxy> connectionTypeComboBox;
+    @FXML
+    private JFXToggleNode showPasswordToggle;
+    @FXML
+    private JFXPasswordField passwordField;
+    @FXML
+    private JFXTextField passwordTextField;
+
     /**
      Initializes the controller class.
      */
     @FXML
     public void initialize() {
-        // TODO
+        connectionTypeComboBox.getItems().setAll(Proxy.values());
+        passwordField.visibleProperty()
+                .bind(showPasswordToggle.selectedProperty().not());
+        passwordTextField.visibleProperty()
+                .bind(showPasswordToggle.selectedProperty());
+        passwordTextField.promptTextProperty()
+                .bind(passwordField.promptTextProperty());
+        passwordTextField.textProperty()
+                .bindBidirectional(passwordField.textProperty());
+    }
+
+    private enum Proxy implements Type {
+
+        NONE("Without proxy"),
+        HTTP("HTTP proxy"),
+        SOCKS4("Socks 4 proxy"),
+        SOCKS5("Socks 5 proxy");
+
+        private final String description;
+
+        private Proxy(String description) {
+            this.description = description;
+        }
+
+        @Override
+        public String getDescription() {
+            return description;
+        }
+
+        @Override
+        public String toString() {
+            return description;
+        }
+
+    }
+
+    private interface Type {
+
+        String getDescription();
     }
 
 }
