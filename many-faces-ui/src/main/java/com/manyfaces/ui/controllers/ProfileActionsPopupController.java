0277e818b092051a204c97b6c03bfbc273b00372
Hiram K <hiram.kamau@outlook.com>
2019-09-27 11:26:22 +0300

    Add functionality
    - Add the selected profile to registry

--- many-faces-ui/src/main/java/com/manyfaces/ui/controllers/ProfileActionsPopupController.java
+++ many-faces-ui/src/main/java/com/manyfaces/ui/controllers/ProfileActionsPopupController.java
@@ -5,11 +5,14 @@ package com.manyfaces.ui.controllers;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPopup;
+import com.manyfaces.model.Profile;
 import com.manyfaces.spi.RootComponent;
 import com.manyfaces.ui.ProfileEditHome;
+import com.manyfaces.ui.ProfileEditHome.EditType;
 import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import org.openide.util.Lookup;
+import com.manyfaces.spi.Registry;
 
 /**
  FXML Controller class
@@ -38,12 +41,17 @@ public class ProfileActionsPopupController {
     public void initialize() {
     }
 
-    void setPopup(JFXPopup popup) {
+    void setProfilePopup(Profile profile, JFXPopup popup) {
         editButton.setOnAction(e -> {
+            //Announce that the profile that was passed 
+            //to this method is the one that all clients
+            //should be keen on from now on
+            LOOKUP.lookup(Registry.class).setAll(profile);
+            
             popup.hide();
-            RootComponent component = LOOKUP.lookup(RootComponent.class);
-            ProfileEditHome edit = new ProfileEditHome("Edit browser profile");
-            component.setContent(edit.getPane());
+            
+            RootComponent rc = LOOKUP.lookup(RootComponent.class);
+            rc.setContent(new ProfileEditHome(EditType.EDIT).getPane());
         });
     }
 
