b7c550d71cb3d6638bdcb73d542ed12db739a64f
Hiram K <hiram.kamau@outlook.com>
2019-09-28 12:26:49 +0300

    Add fonts page open action

--- many-faces-ui/src/main/java/com/manyfaces/ui/controllers/ProfileMenuController.java
+++ many-faces-ui/src/main/java/com/manyfaces/ui/controllers/ProfileMenuController.java
@@ -128,6 +128,7 @@ public class ProfileMenuController {
         proxyToggle.setOnAction(e -> setContentFrom("/views/ProfileProxy.fxml"));
         timezoneToggle.setOnAction(e -> setContentFrom("/views/ProfileTimezone.fxml"));
         webRtcToggle.setOnAction(e -> setContentFrom("/views/ProfileWebRtc.fxml"));
+        fontsToggle.setOnAction(e -> setContentFrom("/views/ProfileAdvancedFonts.fxml"));
         devicesToggle.setOnAction(e -> setContentFrom("/views/ProfileAdvancedDevices.fxml"));
         extensionsToggle.setOnAction(e -> setContentFrom("/views/ProfileAdvancedExtensions.fxml"));
         storageToggle.setOnAction(e -> setContentFrom("/views/ProfileAdvancedStorage.fxml"));
